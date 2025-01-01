1106f8d53d1182f7b229ad34c2e84d17fe70bf3e
Samuel <samuelfesant@gmail.com>
2025-01-01 20:10:32 -0300

    Cria migration para inserts de privilégios

---
+++ src/main/resources/db/migrations/V.1__insert_privilegios.sql
@@ -0,0 +1,158 @@
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_MARCA');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_MARCA');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_MARCA');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_MARCA');
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_SUBCATEGORIA');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_SUBCATEGORIA');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_SUBCATEGORIA');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_SUBCATEGORIA');
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_CATEGORIA');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_CATEGORIA');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_CATEGORIA');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_CATEGORIA');
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_CLIENTE');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_CLIENTE');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_CLIENTE');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_CLIENTE');
+
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_FORNECEDOR');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_FORNECEDOR');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_FORNECEDOR');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_FORNECEDOR');
+
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_ITEM');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_ITEM');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_ITEM');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_ITEM');
+
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_ORDEM');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_ORDEM');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_ORDEM');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_ORDEM');
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_ORDEM_ITEM');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_ORDEM_ITEM');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_ORDEM_ITEM');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_ORDEM_ITEM');
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_PRODUTO');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_PRODUTO');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_PRODUTO');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_PRODUTO');
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_SITUACAO');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_SITUACAO');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_SITUACAO');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_SITUACAO');
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_USUARIO');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_USUARIO');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_USUARIO');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_USUARIO');
+
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'VIEW_ROLE');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'CREATE_ROLE');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'UPDATE_ROLE');
+INSERT INTO public.privilegio
+(id, created_by, created_date, last_modified_by, last_modified_date, descricao)
+VALUES(nextval('seq_privilegio'), 'Admin', now(), 'Admin', now(), 'DELETE_ROLE');
